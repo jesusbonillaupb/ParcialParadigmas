7b6da8459f64dd0d0f8a1ab05d2af014be1540b7
jesusbonillaupb <80067532+jesusbonillaupb@users.noreply.github.com>
2021-11-07 20:07:42 -0500

    commit1

--- ParcialParadigmas/home.Designer.cs
+++ ParcialParadigmas/home.Designer.cs
@@ -29,13 +29,398 @@ namespace ParcialParadigmas
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnE1 = new System.Windows.Forms.Button();
+            this.btnE2 = new System.Windows.Forms.Button();
+            this.btnE3 = new System.Windows.Forms.Button();
+            this.btnE7 = new System.Windows.Forms.Button();
+            this.btnE6 = new System.Windows.Forms.Button();
+            this.btnE5 = new System.Windows.Forms.Button();
+            this.btnE8 = new System.Windows.Forms.Button();
+            this.btnE9 = new System.Windows.Forms.Button();
+            this.btnE4 = new System.Windows.Forms.Button();
+            this.btnE10 = new System.Windows.Forms.Button();
+            this.btnRotarIzquierda = new System.Windows.Forms.Button();
+            this.btnRotarDerecha = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.txtPlaca = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtIndetificacion = new System.Windows.Forms.TextBox();
+            this.desocupar = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.radHombre = new System.Windows.Forms.RadioButton();
+            this.radMujer = new System.Windows.Forms.RadioButton();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.rtbDatos = new System.Windows.Forms.RichTextBox();
+            this.cheAfiliado = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(380, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Original ParkingLot";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnE1
+            // 
+            this.btnE1.Location = new System.Drawing.Point(693, 200);
+            this.btnE1.Name = "btnE1";
+            this.btnE1.Size = new System.Drawing.Size(42, 42);
+            this.btnE1.TabIndex = 1;
+            this.btnE1.Text = "1";
+            this.btnE1.UseVisualStyleBackColor = true;
+            // 
+            // btnE2
+            // 
+            this.btnE2.Location = new System.Drawing.Point(693, 270);
+            this.btnE2.Name = "btnE2";
+            this.btnE2.Size = new System.Drawing.Size(42, 42);
+            this.btnE2.TabIndex = 2;
+            this.btnE2.Text = "2";
+            this.btnE2.UseVisualStyleBackColor = true;
+            // 
+            // btnE3
+            // 
+            this.btnE3.Location = new System.Drawing.Point(693, 340);
+            this.btnE3.Name = "btnE3";
+            this.btnE3.Size = new System.Drawing.Size(42, 42);
+            this.btnE3.TabIndex = 3;
+            this.btnE3.Text = "3";
+            this.btnE3.UseVisualStyleBackColor = true;
+            // 
+            // btnE7
+            // 
+            this.btnE7.Location = new System.Drawing.Point(903, 270);
+            this.btnE7.Name = "btnE7";
+            this.btnE7.Size = new System.Drawing.Size(42, 42);
+            this.btnE7.TabIndex = 4;
+            this.btnE7.Text = "7";
+            this.btnE7.UseVisualStyleBackColor = true;
+            // 
+            // btnE6
+            // 
+            this.btnE6.Location = new System.Drawing.Point(903, 340);
+            this.btnE6.Name = "btnE6";
+            this.btnE6.Size = new System.Drawing.Size(42, 42);
+            this.btnE6.TabIndex = 5;
+            this.btnE6.Text = "6";
+            this.btnE6.UseVisualStyleBackColor = true;
+            // 
+            // btnE5
+            // 
+            this.btnE5.Location = new System.Drawing.Point(833, 340);
+            this.btnE5.Name = "btnE5";
+            this.btnE5.Size = new System.Drawing.Size(42, 42);
+            this.btnE5.TabIndex = 6;
+            this.btnE5.Text = "5";
+            this.btnE5.UseVisualStyleBackColor = true;
+            // 
+            // btnE8
+            // 
+            this.btnE8.Location = new System.Drawing.Point(903, 200);
+            this.btnE8.Name = "btnE8";
+            this.btnE8.Size = new System.Drawing.Size(42, 42);
+            this.btnE8.TabIndex = 7;
+            this.btnE8.Text = "8";
+            this.btnE8.UseVisualStyleBackColor = true;
+            // 
+            // btnE9
+            // 
+            this.btnE9.Location = new System.Drawing.Point(833, 200);
+            this.btnE9.Name = "btnE9";
+            this.btnE9.Size = new System.Drawing.Size(42, 42);
+            this.btnE9.TabIndex = 8;
+            this.btnE9.Text = "9";
+            this.btnE9.UseVisualStyleBackColor = true;
+            // 
+            // btnE4
+            // 
+            this.btnE4.Location = new System.Drawing.Point(763, 340);
+            this.btnE4.Name = "btnE4";
+            this.btnE4.Size = new System.Drawing.Size(42, 42);
+            this.btnE4.TabIndex = 10;
+            this.btnE4.Text = "4";
+            this.btnE4.UseVisualStyleBackColor = true;
+            // 
+            // btnE10
+            // 
+            this.btnE10.Location = new System.Drawing.Point(763, 200);
+            this.btnE10.Name = "btnE10";
+            this.btnE10.Size = new System.Drawing.Size(42, 42);
+            this.btnE10.TabIndex = 11;
+            this.btnE10.Text = "10";
+            this.btnE10.UseVisualStyleBackColor = true;
+            // 
+            // btnRotarIzquierda
+            // 
+            this.btnRotarIzquierda.Location = new System.Drawing.Point(679, 426);
+            this.btnRotarIzquierda.Name = "btnRotarIzquierda";
+            this.btnRotarIzquierda.Size = new System.Drawing.Size(75, 23);
+            this.btnRotarIzquierda.TabIndex = 12;
+            this.btnRotarIzquierda.Text = "<--rotar";
+            this.btnRotarIzquierda.UseVisualStyleBackColor = true;
+            this.btnRotarIzquierda.Click += new System.EventHandler(this.btnRotarIzquierda_Click);
+            // 
+            // btnRotarDerecha
+            // 
+            this.btnRotarDerecha.Location = new System.Drawing.Point(894, 426);
+            this.btnRotarDerecha.Name = "btnRotarDerecha";
+            this.btnRotarDerecha.Size = new System.Drawing.Size(75, 23);
+            this.btnRotarDerecha.TabIndex = 13;
+            this.btnRotarDerecha.Text = "rotar-->";
+            this.btnRotarDerecha.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(68, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "tipo";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Items.AddRange(new object[] {
+            "carro particular",
+            "camioneta",
+            "microbus"});
+            this.cmbTipo.Location = new System.Drawing.Point(71, 200);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(121, 21);
+            this.cmbTipo.TabIndex = 15;
+            this.cmbTipo.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(72, 354);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 16);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "placa";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(68, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 16);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "marca";
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(71, 290);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(121, 20);
+            this.txtMarca.TabIndex = 18;
+            // 
+            // txtPlaca
+            // 
+            this.txtPlaca.Location = new System.Drawing.Point(71, 382);
+            this.txtPlaca.Name = "txtPlaca";
+            this.txtPlaca.Size = new System.Drawing.Size(121, 20);
+            this.txtPlaca.TabIndex = 19;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(67, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(117, 20);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Datos Vehiculo";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(299, 133);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(127, 20);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Datos conductor";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(300, 181);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(153, 16);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "numero de identificacion";
+            // 
+            // txtIndetificacion
+            // 
+            this.txtIndetificacion.Location = new System.Drawing.Point(303, 212);
+            this.txtIndetificacion.Name = "txtIndetificacion";
+            this.txtIndetificacion.Size = new System.Drawing.Size(121, 20);
+            this.txtIndetificacion.TabIndex = 23;
+            // 
+            // desocupar
+            // 
+            this.desocupar.Location = new System.Drawing.Point(225, 446);
+            this.desocupar.Name = "desocupar";
+            this.desocupar.Size = new System.Drawing.Size(75, 23);
+            this.desocupar.TabIndex = 26;
+            this.desocupar.Text = "desocupar";
+            this.desocupar.UseVisualStyleBackColor = true;
+            this.desocupar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(300, 255);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 16);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "sexo";
+            // 
+            // radHombre
+            // 
+            this.radHombre.AutoSize = true;
+            this.radHombre.Location = new System.Drawing.Point(303, 280);
+            this.radHombre.Name = "radHombre";
+            this.radHombre.Size = new System.Drawing.Size(60, 17);
+            this.radHombre.TabIndex = 29;
+            this.radHombre.TabStop = true;
+            this.radHombre.Text = "hombre";
+            this.radHombre.UseVisualStyleBackColor = true;
+            // 
+            // radMujer
+            // 
+            this.radMujer.AutoSize = true;
+            this.radMujer.Location = new System.Drawing.Point(303, 303);
+            this.radMujer.Name = "radMujer";
+            this.radMujer.Size = new System.Drawing.Size(50, 17);
+            this.radMujer.TabIndex = 30;
+            this.radMujer.TabStop = true;
+            this.radMujer.Text = "mujer";
+            this.radMujer.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(336, 446);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 34;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // rtbDatos
+            // 
+            this.rtbDatos.Location = new System.Drawing.Point(435, 693);
+            this.rtbDatos.Name = "rtbDatos";
+            this.rtbDatos.Size = new System.Drawing.Size(534, 120);
+            this.rtbDatos.TabIndex = 36;
+            this.rtbDatos.Text = "";
+            // 
+            // cheAfiliado
+            // 
+            this.cheAfiliado.AutoSize = true;
+            this.cheAfiliado.Location = new System.Drawing.Point(303, 382);
+            this.cheAfiliado.Name = "cheAfiliado";
+            this.cheAfiliado.Size = new System.Drawing.Size(59, 17);
+            this.cheAfiliado.TabIndex = 37;
+            this.cheAfiliado.Text = "afiliado";
+            this.cheAfiliado.UseVisualStyleBackColor = true;
+            // 
+            // home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1060, 586);
+            this.Controls.Add(this.cheAfiliado);
+            this.Controls.Add(this.rtbDatos);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.radMujer);
+            this.Controls.Add(this.radHombre);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.desocupar);
+            this.Controls.Add(this.txtIndetificacion);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPlaca);
+            this.Controls.Add(this.txtMarca);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnRotarDerecha);
+            this.Controls.Add(this.btnRotarIzquierda);
+            this.Controls.Add(this.btnE10);
+            this.Controls.Add(this.btnE4);
+            this.Controls.Add(this.btnE9);
+            this.Controls.Add(this.btnE8);
+            this.Controls.Add(this.btnE5);
+            this.Controls.Add(this.btnE6);
+            this.Controls.Add(this.btnE7);
+            this.Controls.Add(this.btnE3);
+            this.Controls.Add(this.btnE2);
+            this.Controls.Add(this.btnE1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbTipo);
+            this.Name = "home";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.home_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnE1;
+        private System.Windows.Forms.Button btnE2;
+        private System.Windows.Forms.Button btnE3;
+        private System.Windows.Forms.Button btnE7;
+        private System.Windows.Forms.Button btnE6;
+        private System.Windows.Forms.Button btnE5;
+        private System.Windows.Forms.Button btnE8;
+        private System.Windows.Forms.Button btnE9;
+        private System.Windows.Forms.Button btnE4;
+        private System.Windows.Forms.Button btnE10;
+        private System.Windows.Forms.Button btnRotarIzquierda;
+        private System.Windows.Forms.Button btnRotarDerecha;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.TextBox txtPlaca;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtIndetificacion;
+        private System.Windows.Forms.Button desocupar;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RadioButton radHombre;
+        private System.Windows.Forms.RadioButton radMujer;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.RichTextBox rtbDatos;
+        private System.Windows.Forms.CheckBox cheAfiliado;
     }
 }
 
--- ParcialParadigmas/home.cs
+++ ParcialParadigmas/home.cs
@@ -12,9 +12,174 @@ namespace ParcialParadigmas
 {
     public partial class home : Form
     {
+        
+        
         public home()
         {
             InitializeComponent();
         }
+
+        private void home_Load(object sender, EventArgs e)
+        {
+            
+        }
+        int i = 0;
+        private void btnRotarIzquierda_Click(object sender, EventArgs e)
+        {
+            var btnX = btnE1.Location.X;
+            var btnY= btnE1.Location.Y;
+
+            this.btnE1.Location = new Point(btnE2.Location.X, btnE2.Location.Y);
+                this.btnE2.Location = new Point(btnE3.Location.X, btnE3.Location.Y);
+                this.btnE3.Location = new Point(btnE4.Location.X, btnE4.Location.Y);
+                this.btnE4.Location = new Point(btnE5.Location.X, btnE5.Location.Y);
+                this.btnE5.Location = new Point(btnE6.Location.X, btnE6.Location.Y);
+                this.btnE6.Location = new Point(btnE7.Location.X, btnE7.Location.Y);
+                this.btnE7.Location = new Point(btnE8.Location.X, btnE8.Location.Y);
+                this.btnE8.Location = new Point(btnE9.Location.X, btnE9.Location.Y);
+                this.btnE9.Location = new Point(btnE10.Location.X, btnE10.Location.Y);
+                this.btnE10.Location = new Point(btnX, btnY);
+
+        }
+        
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtIndetificacion.Clear();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Parqueadero generar = new Parqueadero();
+            generar.GenerarParkingSpot();
+
+
+            txtPlaca.Clear();
+            txtMarca.Clear();
+            txtIndetificacion.Clear();
+            var t = cmbTipo.Text;
+            var m = txtMarca.Text;
+            var p = txtPlaca.Text;
+            var i = txtIndetificacion.Text;
+            var s = "";
+            var ho = radHombre.Checked;
+            var mu = radMujer.Checked;
+            var a = "";
+            var afi = cheAfiliado.Checked;
+            if (afi)
+            {
+                a = "y que esta afiliado";
+
+            }
+            if (ho)
+            {
+                s= "masculino";
+
+            }
+            if (mu)
+            {
+                s = "femnino";
+
+            }
+
+            a+= "el " + t + " de marca " + m + "con placa " + p + "del propietario con cc= " + i + " de sexo " + s + a;
+            
+            
+        }
     }
+
+    public class Parqueadero
+    {
+        
+        public int Capcidad { get; set; }
+        public int Disponibles { get; set; }
+        public int Ocupados{ get; set; }
+    List<ParkingSpot> Espacios = new List<ParkingSpot>();
+        
+
+        public void GenerarParkingSpot()
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                Espacios.Add(new ParkingSpot());
+            }
+           
+            Espacios[0].Disponible=true;
+           //sirve para generar los Parking Spots del parqueadero
+        }
+        public void VerificarDisponibilidad() {
+        
+            //sirvira para verificar la disponibilidad de un espacio
+        //ej: if espacio1 or espacio2 or espacio3 ... disponibilidad= true, seguir con el metodo AsignarParkingSpot()
+        //ej: if espacio1 or espacio2 or espacio3 ... disponibilidad= false envar mensaje "no hay espacios disponibles"
+        
+        }
+        public void AsignarParkingSpot()
+        {
+            //sirvira para asignar un carro a un determinado espacio disponible
+            //como se dijo en el pdf se a se asigna el punto de parqueo más cercano en la banda que este vacio 
+            //ej: if espacio1.Disponible= true, asignar carro a espacio1
+            //luego se ingresara a ese veiculo
+
+        }
+        
+
+
+    }
+    public class ParkingSpot
+    {
+        public bool Disponible { get; set; }
+        public int Numerodeestacionamiento { get; set; }
+        public string DatosVehiculoYconductor{ get; set; }
+
+        public void IngresarVehiculo()
+        {
+            //sirvira para ingresar el vehiculo a un determinado estacionamiento y que el espacio de esta se marque en ocupado
+        }
+        public void RetirarrVehiculo()
+        {
+            //sirvira para ingresar el vehiculo a un determinado estacionamiento y que el espacio de esta se marque en libre
+        }
+    }
+    public class Vehicle
+    {
+        string Tipo;
+        string Placa;
+        string Marca;
+        
+    }
+    public class driver
+    {
+        int Indentificacion;
+        string Sexo;
+        bool Afiliado;
+       
+        
+    }
+    //nota:depronto se pueda crear una clase tarifa la cual tenga atributos la hora de entrada, la hora de salida, el precio por minuto y el incremento
+    //posibles metodos: calcular tarifa 
 }
